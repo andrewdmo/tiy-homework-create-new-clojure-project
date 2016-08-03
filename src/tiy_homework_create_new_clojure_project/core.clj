ff08db0d988bb4bbbb02301f0c849b796d2a747d
andrewdmo <andrewdmo@gmail.com>
2016-08-03 18:57:03 -0400

    First try

---
+++ src/tiy_homework_create_new_clojure_project/core.clj
@@ -0,0 +1,31 @@
+(ns tiy-homework-create-new-clojure-project.core)
+
+(defn -main[]
+
+  (def a "Booya!")
+
+  (def b "Guess...")
+
+  (def c "what")
+
+  (def d "seems")
+
+  (def e "to")
+
+  (def f "be")
+
+  (def g "working!")
+
+  (println a)
+
+  (println b c d e f g)
+
+  (def h 1)
+
+  (def i 2)
+
+  (def j 3)
+
+  (println "...as evidenced by...math!" (+ 1 2 3))
+
+  )
